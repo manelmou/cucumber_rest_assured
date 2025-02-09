a559a49ade5c0ea9e23406b033beda1f783c44c7
Manel <manel.mou@gmail.com>
2025-02-09 18:41:28 +0100

    correct GetMessages and GetMessageCount Tests

--- src/test/java/com/stepsDefinitions/GetMessageCount.java
+++ src/test/java/com/stepsDefinitions/GetMessageCount.java
@@ -1,10 +1,9 @@
 package com.stepsDefinitions;
 
-import java.util.List;
-
 import org.json.JSONObject;
 import org.junit.Assert;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
@@ -13,24 +12,25 @@ import io.restassured.response.Response;
 public class GetMessageCount {
 
 	private Response response;
-	private static final String BASE_URL = "https://automationintesting.online/message/";
+	private static final String BASE_URL = "https://automationintesting.online/message/count";
 	JSONObject body = new JSONObject();
 
-	@When("I send a GET request to fetch all messages")
-	public void i_send_a_get_request_to_fetch_all_messages() {
+	@When("I send a GET request to fetch number of messages")
+	public void i_send_a_get_request_to_fetch_number_of_messages() {
 		response = RestAssured.given().when().get(BASE_URL);
 		response.print();
 	}
 
-	@Then("the response status should be 200")
-	public void the_response_status_should_be_200() {
+	@Then("the response count status should be 200")
+	public void the_response_count_status_should_be_200() {
 		Assert.assertEquals("Expected status code 200", 200, response.getStatusCode());
 	}
 
-	@Then("the response should contain a list of messages")
-	public void the_response_should_contain_a_list_of_messages() {
-		Assert.assertNotNull("Response is null!", response);
-		List<?> messages = response.jsonPath().getList("messages");
-		Assert.assertFalse("The list of messages is empty!", messages.isEmpty());
+	@And("the response should containthe number of messages")
+	public void the_response_should_contain_a_number_of_messages() {
+		String count = response.jsonPath().getString("count");
+		Assert.assertNotNull("Number is null!", count);
+		Assert.assertTrue("the number is incorrect", Integer.parseInt(count) > 0);
+
 	}
 }
--- src/test/java/com/stepsDefinitions/GetMessages.java
+++ src/test/java/com/stepsDefinitions/GetMessages.java
@@ -1,37 +1,36 @@
 package com.stepsDefinitions;
 
+import java.util.List;
+
 import org.json.JSONObject;
 import org.junit.Assert;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class GetMessages {
-
 	private Response response;
-	private static final String BASE_URL = "https://automationintesting.online/message/count";
+	private static final String BASE_URL = "https://automationintesting.online/message/";
 	JSONObject body = new JSONObject();
 
-	@When("I send a GET request to fetch number of messages")
-	public void i_send_a_get_request_to_fetch_number_of_messages() {
+	@When("I send a GET request to fetch all messages")
+	public void i_send_a_get_request_to_fetch_all_messages() {
 		response = RestAssured.given().when().get(BASE_URL);
 		response.print();
 	}
 
-	@Then("the response count status should be 200")
-	public void the_response_count_status_should_be_200() {
+	@Then("the response status should be 200")
+	public void the_response_status_should_be_200() {
 		Assert.assertEquals("Expected status code 200", 200, response.getStatusCode());
 	}
 
-	@And("the response should containthe number of messages")
-	public void the_response_should_contain_a_number_of_messages() {
-		String count = response.jsonPath().getString("count");
-		Assert.assertNotNull("Number is null!", count);
-		Assert.assertTrue("the number is incorrect", Integer.parseInt(count) > 0);
-
+	@Then("the response should contain a list of messages")
+	public void the_response_should_contain_a_list_of_messages() {
+		Assert.assertNotNull("Response is null!", response);
+		List<?> messages = response.jsonPath().getList("messages");
+		Assert.assertFalse("The list of messages is empty!", messages.isEmpty());
 	}
 
 }
